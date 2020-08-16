32a955ab3d4ccd87483cd921341c42bd87d50465
Ryan Hartlage <ryanplusplus@gmail.com>
2020-08-16 14:11:10 -0400

    Automagically patch FreeRTOS for SystemView

--- src/FreeRTOSConfig.h
+++ src/FreeRTOSConfig.h
@@ -76,7 +76,7 @@ extern uint32_t SystemCoreClock;
 #define configMAX_CO_ROUTINE_PRIORITIES (2)
 
 /* Software timer definitions. */
-#define configUSE_TIMERS 1
+#define configUSE_TIMERS 0
 #define configTIMER_TASK_PRIORITY (2)
 #define configTIMER_QUEUE_LENGTH 5
 #define configTIMER_TASK_STACK_DEPTH (80)
--- target.mk
+++ target.mk
@@ -5,16 +5,23 @@ CPU := cortex-m0
 ARCH := armv6-m
 OPENOCD_CFG := openocd
 LINKER_CFG := linker.ld
+USE_SYSTEM_VIEW := Y
 
 DEFINES := \
   STM32F051x8 \
   HSE_VALUE=8000000 \
 
+ifeq ($(USE_SYSTEM_VIEW),Y)
+FREERTOS := lib/patched/FreeRTOS-Kernel
+else
+FREERTOS := lib/FreeRTOS-Kernel
+endif
+
 SRC_FILES := \
 
 SRC_DIRS := \
-  lib/FreeRTOS-Kernel \
-  lib/FreeRTOS-Kernel/portable/GCC/ARM_CM0 \
+  $(FREERTOS) \
+  $(FREERTOS)/portable/GCC/ARM_CM0 \
   src \
   src/app \
   src/hardware \
@@ -26,14 +33,16 @@ LIB_DIRS := \
   lib/tiny/src \
 
 INC_DIRS := \
-  lib/FreeRTOS-Kernel/include \
+  $(FREERTOS)/include \
   lib/stm32cube/CMSIS/ARM/inc \
   lib/stm32cube/CMSIS/STM32F0xx/inc \
   lib/stm32cube/HAL/STM32F0xx/inc \
   lib/tiny/include \
 
+IGNORE := $(shell $(MAKE) --no-print-directory -f patch.mk)
+
 include makefile-worker.mk
 
 .PHONY: watch
 watch:
-	rerun "$(MAKE) --no-print-directory -f $(firstword $(MAKEFILE_LIST))"
+	@rerun "$(MAKE) --no-print-directory -f $(firstword $(MAKEFILE_LIST))"
