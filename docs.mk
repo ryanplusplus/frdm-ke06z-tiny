c21fc33ac78240ab33925be9994d4f4413966f86
Ryan Hartlage <ryanplusplus@gmail.com>
2021-02-21 14:14:10 -0500

    Marked hardware_manual target as PHONY

--- docs.mk
+++ docs.mk
@@ -1,6 +1,7 @@
 HARDWARE_MANUAL_URL := https://www.nxp.com/files-static/microcontrollers/doc/ref_manual/MKE06P80M48SF0RM.pdf
 DATASHEET_URL := https://www.nxp.com/docs/en/data-sheet/MKE06P80M48SF0.pdf
 
+.PHONY: hardware_manual
 hardware_manual:
 	xdg-open $(HARDWARE_MANUAL_URL)
 
