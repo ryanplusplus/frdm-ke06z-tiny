5a2bb81ab4e31de88c760a11b2780713f6a56b0f
Ryan Hartlage <ryanplusplus@gmail.com>
2022-01-17 17:37:23 -0500

    Split flags into a separate cmake file

---
+++ cmake/flags.cmake
@@ -0,0 +1,33 @@
+enable_language(C ASM)
+
+set(COMPILE_AND_LINK_OPTIONS
+  --specs=nano.specs
+  -mcpu=cortex-m0plus
+  -mthumb
+  -g
+  -g2
+  -Os
+  -fdata-sections
+  -ffunction-sections
+  -Wall
+  -Wextra
+  -Werror
+  -Wfatal-errors
+  -Wcast-qual
+  -pedantic
+  -nostartfiles
+  -std=c99
+)
+
+add_compile_options(
+  ${COMPILE_AND_LINK_OPTIONS}
+)
+
+add_link_options(
+  ${COMPILE_AND_LINK_OPTIONS}
+  -Wl,--gc-sections
+  -Wl,-Map=${PROJECT_NAME}.map
+  -T ../linker.ld
+)
+
+set(CMAKE_INTERPROCEDURAL_OPTIMIZATION TRUE)
